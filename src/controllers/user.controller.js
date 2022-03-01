4768c9381064b6b0bbdf40ab656932756bd45803
salavudeenH <salavudeenhadji@gmail.com>
2022-03-01 14:58:09 +0100

    he

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const configs = require("../configs");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
