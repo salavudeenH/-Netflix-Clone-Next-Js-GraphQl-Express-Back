afa6e567fb155ce600cc5d40113d9cb8b4b58421
Adarsh Srivastava <adarshsrivastava.tech@gmail.com>
2022-03-01 21:48:26 +0530

    User Controller Changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user.model");
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const configs = require("../configs");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -13,7 +13,7 @@ exports.register = (req, res) => {
     email: req.body.email,
     isAdmin: false,
     password: hashedPassword,
-    isSuscribe:false
+    isSuscribe: false,
   });
 
   user
@@ -38,15 +38,14 @@ exports.register = (req, res) => {
       res.status(500).send({
         message: err.message || "Some error occured",
       });
-
     });
 };
 
 exports.login = (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   User.findOne({ email: req.body.email })
     .then((user) => {
-      console.log(user)
+      console.log(user);
       let passwordValid = bcrypt.compareSync(req.body.password, user.password);
       if (!passwordValid) {
         return res.status(401).send({
@@ -68,16 +67,19 @@ exports.login = (req, res) => {
       res.status(200).send({
         auth: true,
         token: userToken,
+        ...user._doc,
       });
     })
-    .catch((err) => res.send({ message: "L'adresse mail ou le mot de passe est invalide"}));
+    .catch((err) =>
+      res.send({ message: "L'adresse mail ou le mot de passe est invalide" })
+    );
 };
 
 exports.getUser = (req, res) => {
   console.log(req.user);
   User.findById(req.user.id)
-      .then((user) => {
-          console.log(user);
+    .then((user) => {
+      console.log(user);
       res.send(user);
     })
     .catch((err) => res.status(404).send(err));
@@ -95,51 +97,51 @@ exports.updateUser = (req, res) => {
 
 exports.verifyToken = (req, res) => {
   if (req.user) {
-      res.status(200).json({verify:true})
+    res.status(200).json({ verify: true });
   }
-}
+};
 
 exports.payment = async (req, res, next) => {
-    const { plan } = req.query;
-    const storeItems = new Map([
-      [0, { priceInEUR: 30, name: "Essential" }],
-      [1, { priceInEUR: 60, name: "Standard" }],
-      [2, { priceInEUR: 100, name: "Premium" }],
-    ]);
-    try {
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        mode: "payment",
-        success_url: `http://localhost:3000/confirmation?plan=${plan}`,
-        cancel_url: `http://localhost:3000/cancel?plan=${plan}`,
-        line_items: req.body.items.map((item) => {
-          const storeItem = storeItems.get(item.id);
-          return {
-            price_data: {
-              currency: "eur",
-              product_data: {
-                name: storeItem.name,
-              },
-              unit_amount: storeItem.priceInEUR * 100,
+  const { plan } = req.query;
+  const storeItems = new Map([
+    [0, { priceInEUR: 30, name: "Essential" }],
+    [1, { priceInEUR: 60, name: "Standard" }],
+    [2, { priceInEUR: 100, name: "Premium" }],
+  ]);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      success_url: `http://localhost:3000/confirmation?plan=${plan}`,
+      cancel_url: `http://localhost:3000/cancel?plan=${plan}`,
+      line_items: req.body.items.map((item) => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "eur",
+            product_data: {
+              name: storeItem.name,
             },
-            quantity: item.quantity,
-          };
-        }),
-      });
-      res.json({ url: session.url });
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-  };
-  
-  exports.verifyPayment = async (req, res, next) => {
-    try {
-      const user = await User.findById({id: req.body.id });
-      user.isSuscribe = true;
-      user.suscribeAt = Date.now();
-      await user.save();
-      res.json({ user: user, err: false });
-    } catch (err) {
-      res.status(400).json({ err: true });
-    }
-  };
+            unit_amount: storeItem.priceInEUR * 100,
+          },
+          quantity: item.quantity,
+        };
+      }),
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+exports.verifyPayment = async (req, res, next) => {
+  try {
+    const user = await User.findById({ id: req.body.id });
+    user.isSuscribe = true;
+    user.suscribeAt = Date.now();
+    await user.save();
+    res.json({ user: user, err: false });
+  } catch (err) {
+    res.status(400).json({ err: true });
+  }
+};
