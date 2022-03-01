8dd3ed6fc556299ca498170c21a4e013f6ff7371
salavudeenH <salavudeenhadji@gmail.com>
2022-03-02 00:21:57 +0100

    solve

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -112,8 +112,8 @@ exports.payment = async (req, res, next) => {
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         mode: "payment",
-        success_url: `http://localhost:3000/confirmation?plan=${userID}`,
-        cancel_url: `http://localhost:3000/cancel?plan=${userID}`,
+        success_url: `https://netflix-a63k4dqsb-salavudeen27-gmailcom.vercel.app/confirmation${userID}`,
+        cancel_url: `https://netflix-a63k4dqsb-salavudeen27-gmailcom.vercel.app/cancel?plan=${userID}`,
         line_items: req.body.items.map((item) => {
           const storeItem = storeItems.get(item.id);
           return {
